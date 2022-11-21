0d8a02f4ec234355c8f77d155a46aa3b1fc52229
cvalencia1991 <cesar4a6z@gmail.com>
2022-11-21 17:11:35 -0500

    Edit Navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,29 +1,45 @@
 import { Link } from 'react-router-dom';
+import Search from './components/Search';
 import logo from './imgs/Logo_ARTCHICAGO.jpg';
 
 function Navbar() {
-  const links = [{
+  const back = [{
     id: 1,
     path: '/',
-    text: 'Gallery',
-  },
-  {
-    id: 2,
-    path: '/Aboutme',
-    text: 'Aboutme',
-  },
-  {
-    id: 3,
-    path: '/References',
-    text: 'References',
+    text: '<',
   },
   ];
+  const links = [
+    {
+      id: 0,
+      path: '/Details',
+      text: 'Details',
+    },
+    {
+      id: 1,
+      path: '/About me',
+      text: 'About me',
+    },
+    {
+      id: 2,
+      path: '/References',
+      text: 'References',
+    },
+  ];
   return (
-    <nav>
-      <div>
-        <img src={logo} alt="" />
+    <nav className="Navstyle">
+      <div className="backbutton">
+        {back.map((backinfo) => (
+          <Link key={backinfo.id} to={backinfo.path}>{backinfo.text}</Link>
+        ))}
       </div>
-      <ul>
+      <div>
+        {back.map((backinfo) => (
+          <Link key={backinfo.id} to={backinfo.path}><img className="stylelogo" src={logo} alt="" /></Link>
+        ))}
+      </div>
+      <Search />
+      <ul className="styleMenu">
         {links.map((link) => (
           <li key={link.id}>
             <Link to={link.path}>{link.text}</Link>
